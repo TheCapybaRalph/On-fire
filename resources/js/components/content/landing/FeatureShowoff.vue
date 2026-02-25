ef54485ca2f23996694b1da6647b9aebaef1d161
KuroUsagi1010 <newtsullivan@gmail.com>
2026-02-24 23:28:32 -0500

    added banner hehehe

---
+++ resources/js/components/content/landing/FeatureShowoff.vue
@@ -0,0 +1,221 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="flex min-h-[80vh] justify-center">
+        <div class="container-sm p-5">
+            <div class="relative">
+                <div
+                    class="relative rounded-lg bg-lp-background shadow-lp-primary"
+                >
+                    <div class="grid grid-cols-2">
+                        <div class="p-16">
+                            <p
+                                class="mb-10 text-6xl font-bold tracking-tight text-white"
+                            >
+                                Multiple Notification <br />
+                                Channels
+                            </p>
+                            <div class="ml-5 flex flex-col gap-4 text-gray-400">
+                                <div class="flex items-center gap-4">
+                                    <div
+                                        class="rounded-xl bg-lp-primary p-2 text-white"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="size-5"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <p class="text-sm font-light tracking-wide">
+                                        Connect notifications to pages
+                                    </p>
+                                </div>
+                                <div class="flex items-center gap-4">
+                                    <div
+                                        class="rounded-xl bg-lp-primary p-2 text-white"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="size-5"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <p class="text-sm font-light tracking-wide">
+                                        Assign to users or teams
+                                    </p>
+                                </div>
+                                <div class="flex items-center gap-4">
+                                    <div
+                                        class="rounded-xl bg-lp-primary p-2 text-white"
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            stroke-width="1.5"
+                                            stroke="currentColor"
+                                            class="size-5"
+                                        >
+                                            <path
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+                                            />
+                                        </svg>
+                                    </div>
+                                    <p class="text-sm font-light tracking-wide">
+                                        Include runbooks for guidance
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            class="bg-radial from-lp-accent to-lp-background to-60% p-12"
+                        >
+                            <div
+                                class="flex h-full w-full flex-col justify-center gap-8"
+                            >
+                                <div
+                                    class="flex w-fit animate-pulse items-center gap-4 self-end rounded-md border border-gray-400 bg-white/10 p-4 text-gray-400"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        class="size-5"
+                                    >
+                                        <rect
+                                            width="3"
+                                            height="8"
+                                            x="13"
+                                            y="2"
+                                            rx="1.5"
+                                        />
+                                        <path
+                                            d="M19 8.5V10h1.5A1.5 1.5 0 1 0 19 8.5"
+                                        />
+                                        <rect
+                                            width="3"
+                                            height="8"
+                                            x="8"
+                                            y="14"
+                                            rx="1.5"
+                                        />
+                                        <path
+                                            d="M5 15.5V14H3.5A1.5 1.5 0 1 0 5 15.5"
+                                        />
+                                        <rect
+                                            width="8"
+                                            height="3"
+                                            x="14"
+                                            y="13"
+                                            rx="1.5"
+                                        />
+                                        <path
+                                            d="M15.5 19H14v1.5a1.5 1.5 0 1 0 1.5-1.5"
+                                        />
+                                        <rect
+                                            width="8"
+                                            height="3"
+                                            x="2"
+                                            y="8"
+                                            rx="1.5"
+                                        />
+                                        <path
+                                            d="M8.5 5H10V3.5A1.5 1.5 0 1 0 8.5 5"
+                                        />
+                                    </svg>
+                                    <p class="text-xs">
+                                        Your page:
+                                        https://example.com/new-campaign
+                                        <br />
+                                        is on fire
+                                    </p>
+                                </div>
+                                <div
+                                    class="flex w-fit items-center gap-4 rounded-md border border-gray-400 bg-white/10 p-4 text-gray-400"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        class="size-5"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+                                        />
+                                    </svg>
+                                    <p class="text-xs">
+                                        Your Campaign: NewProductsQ1 is having
+                                        an issue<br />
+                                        refer to runbook:
+                                        https://custom-runbook.com/npq1-what-to-do
+                                    </p>
+                                </div>
+                                <div
+                                    class="flex w-fit items-center gap-4 self-center rounded-md border border-gray-400 bg-white/10 p-4 text-gray-400"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        class="size-5"
+                                    >
+                                        <path
+                                            d="M2.992 16.342a2 2 0 0 1 .094 1.167l-1.065 3.29a1 1 0 0 0 1.236 1.168l3.413-.998a2 2 0 0 1 1.099.092 10 10 0 1 0-4.777-4.719"
+                                        />
+                                        <path d="M8 12h.01" />
+                                        <path d="M12 12h.01" />
+                                        <path d="M16 12h.01" />
+                                    </svg>
+                                    <p class="text-xs">
+                                        Your page:
+                                        https://example.com/new-campaign
+                                        <br />
+                                        is on fire
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ resources/js/components/content/landing/StatSwiper.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+</script>
+
+<template>
+    <div class="bg-lp-background shadow-lg shadow-lp-primary/50">
+        <Splide
+            class="border-y border-y-lp-accent/40 bg-lp-accent"
+            :extensions="{ AutoScroll }"
+            :options="{
+                type: 'loop',
+                drag: 'free',
+                perPage: 4,
+                pagination: false,
+                arrows: false,
+                autoScroll: {
+                    speed: 1,
+                    pauseOnHover: false,
+                    pauseOnFocus: false,
+                },
+            }"
+            aria-label="My Favorite Images"
+        >
+            <SplideSlide>
+                <div
+                    class="flex items-center justify-center gap-2 p-5 text-white"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6 text-lp-primary"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                        />
+                    </svg>
+                    <p class="font-bold">GLOBAL NODES: 24/7 ACTIVE</p>
+                </div>
+            </SplideSlide>
+            <SplideSlide>
+                <div
+                    class="flex items-center justify-center gap-2 p-5 text-white"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6 text-lp-primary"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
+                        />
+                    </svg>
+                    <p class="font-bold">UPTIME: 99.99%</p>
+                </div>
+            </SplideSlide>
+            <SplideSlide>
+                <div
+                    class="flex items-center justify-center gap-2 p-5 text-white"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6 text-lp-primary"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64"
+                        />
+                    </svg>
+                    <p class="font-bold">INCIDENTS REPORTED: 1.2M+</p>
+                </div>
+            </SplideSlide>
+            <SplideSlide>
+                <div
+                    class="flex items-center justify-center gap-2 p-5 text-white"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-6 text-lp-primary"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+                        />
+                    </svg>
+                    <p class="font-bold">ACTIVE PAGE MONITORS: 12K</p>
+                </div>
+            </SplideSlide>
+        </Splide>
+    </div>
+</template>
+
+<style scoped></style>
